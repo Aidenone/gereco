2b48f4dbb4a511d348107ae22f810efff4c818f9
System Administrator <root@MacBook-Pro-de-Antoine-2.local>
2023-11-09 18:19:28 +0100

    test notification interval

--- src/main.js
+++ src/main.js
@@ -9,16 +9,33 @@ app.config.globalProperties.currIp = 'https://ns3359877.ip-37-187-97.eu'
 app.component("VueSignaturePad", VueSignaturePad);
 app.use(router).mount('#app')
 
-// function checkDP() {
-// 	Notification.requestPermission().then((permission) => {
-//         console.log(permission);
-//         if (permission === "granted") {
-//             const notification = new Notification("Test", { body: "test notif" });
-//             console.log(notification);
-//         }
-//     });
-// }
-// setInterval(checkDP, 4000);
+function checkDP() {
+	// Notification.requestPermission().then((permission) => {
+ //        console.log(permission);
+ //        if (permission === "granted") {
+ //            const notification = new Notification("Test", { body: "test notif" });
+ //            console.log(notification);
+ //        }
+ //    });
+
+    navigator.serviceWorker.register('service-worker.js');
+	if (Notification.permission === "granted") {
+		navigator.serviceWorker.ready.then(function(registration) {
+			registration.showNotification('Notification with ServiceWorker');
+		});
+	} else {
+		// We need to ask the user for permission
+		Notification.requestPermission().then((permission) => {
+			console.log(permission);
+			if (permission === "granted") {
+				navigator.serviceWorker.ready.then(function(registration) {
+					registration.showNotification('Notification with ServiceWorker');
+				});
+			}
+		});
+	}
+}
+setInterval(checkDP, 4000);
 
 
 // async function test() {
